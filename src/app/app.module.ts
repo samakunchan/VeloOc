f4457584e01c292e2b172be831082934cc848bc6
samakunchan <badgehades@hotmail.com>
2019-12-24 10:32:34 +0100

    feat: initialisation
    - Installation de Husky
    - Installation de commintlint
    - Ajout config de commintlint
    - Installation de mdBoostrap
    - Config mdBootrap dans appModule
    - Update angular.json
    - Update README.md
    - Ajout SAFECOMMIT.md

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, MDBBootstrapModule.forRoot()],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
