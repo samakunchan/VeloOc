3e66107cd72392d7a97328e36574fbcda9bcbc14
samakunchan <badgehades@hotmail.com>
2019-12-28 10:08:49 +0100

    feat: template
    - Ajout du component header
    - Ajout du component slider
    - Ajout du component map
    - Ajout du component reservation
    - Ajout du component canvas
    - Ajout du component compteur
    - Ajout du component footer
    - Ajout du services compteur
    - Ajout du services compteur
    - Ajout du services compteur
    - Modification de app.component.html pour intégrer les components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { HeaderComponent } from './pages/header/header.component';
+import { SliderComponent } from './pages/slider/slider.component';
+import { MapComponent } from './pages/map/map.component';
+import { ReservationComponent } from './pages/reservation/reservation.component';
+import { CanvasComponent } from './pages/canvas/canvas.component';
+import { CompteurComponent } from './pages/compteur/compteur.component';
+import { FooterComponent } from './pages/footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SliderComponent,
+    MapComponent,
+    ReservationComponent,
+    CanvasComponent,
+    CompteurComponent,
+    FooterComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, MDBBootstrapModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/pages/compteur/compteur.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-compteur',
+  templateUrl: './compteur.component.html',
+  styleUrls: ['./compteur.component.scss'],
+})
+export class CompteurComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
